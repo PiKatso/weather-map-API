53adc9c41765ac54587c25d7b83e03867d468051
Kat Gifford and Steven Galvin <kat@>
2017-05-25 13:37:58 -0700

    add functional mapping

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,6 +1,5 @@
 var googKey = require('./../.env').googKey;
 var weaKey = require('./../.env').weaKey;
-var mapsapi = require('google-maps-api')(googKey);
 
 function Map(){
 }
@@ -10,21 +9,13 @@ function LatLng(lat, lon){
 
 Map.prototype.getMap = function(city) {
   var map;
-  function getLocation(city) {
-    // var result = []
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weaKey).then(function(response) {
-      var userLatLng = new google.maps.LatLng(response.coord.lat, response.coord.lon);
-      // var cityLat = response.coord.lat;
-      // result.push(cityLat);
-      // var cityLon = response.coord.lon;
-      // result.push(cityLon);
-      var myCity = {
-        center: userLatLng,
-        zoom: 15
-      };
-    });
-  }
-  mapsapi().then( function( maps ) {
+  var myCity;
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weaKey).then(function(response) {
+    var userLatLng = new google.maps.LatLng(response.coord.lat, response.coord.lon);
+    myCity = {
+      center: userLatLng,
+      zoom: 13
+    };
     map = new google.maps.Map(document.getElementById('map'), myCity);
   });
 };
