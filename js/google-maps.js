5c4837081d015bb172eb1916bbcb68e8a060b669
Kat Gifford and Steven Galvin <steven@>
2017-05-25 14:34:03 -0700

    add marker to map API

--- js/google-maps.js
+++ js/google-maps.js
@@ -3,13 +3,15 @@ var weaKey = require('./../.env').weaKey;
 
 function Map(){
 }
-
 function LatLng(lat, lon){
+}
+function Marker(lat, lon){
 }
 
 Map.prototype.getMap = function(city) {
   var map;
   var myCity;
+  // var marker;
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + weaKey).then(function(response) {
     var userLatLng = new google.maps.LatLng(response.coord.lat, response.coord.lon);
     myCity = {
@@ -17,6 +19,10 @@ Map.prototype.getMap = function(city) {
       zoom: 13
     };
     map = new google.maps.Map(document.getElementById('map'), myCity);
+    new google.maps.Marker({
+      position: userLatLng,
+      map: map
+    });
   });
 };
 
